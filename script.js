bccfc67238a9decd657d306846de0183ed4a5334
Spencer Lyons <sjl8r8@gmail.com>
2019-11-25 12:12:43 -0600

    font color change when active is true now works

--- script.js
+++ script.js
@@ -10,13 +10,16 @@ window.addEventListener("load", function() {
                <h3>${json[i].firstName + " " + json[i].lastName}</h3>
                <ul>
                   <li>Hours in space: ${json[i].hoursInSpace}</li>
-                  <li>Active: ${json[i].active}</li>
+                  <li id="active${json[i].id}">Active: ${json[i].active}</li>
                   <li>Skills: ${json[i].skills}</li>
                </ul>
             </div>
             <img class="avatar" src=${json[i].picture}>
          </div>
          `
+         if (json[i].active === true) {
+             document.getElementById(`active${json[i].id}`).style.color = 'green';
+         }
          
           }
           document.getElementById('count').innerHTML = `Astronaut Count: ${json.length}`;
